958b0db24dd04f420e57c80110d2bc05a540b269
TylersDurden <TylersDurden>
2018-02-28 16:52:14 +0000

    improving login mechanism

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #sudo pip install twitter
+# To install on your machine easily:
+twitter -h 
+# Enter the given code when prompted
+# This will retrieve and save your OAuth tokens locally! 
 cd /projects/Social/Social
 chmod +x main.py
+# now execute the program
 python main.py 
